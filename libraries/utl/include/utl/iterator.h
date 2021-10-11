0f7ac2305c857c7df4b8cb7fa72bf2072808e1c7
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2021-10-11 09:27:34 +0200

    always inline attribute syntax

--- libraries/utl/include/utl/iterator.h
+++ libraries/utl/include/utl/iterator.h
@@ -8,25 +8,25 @@ public:
 
     /* gcc can be stupid at times and generate bad code if it chooses - for whatever reason - not to
      * inline this (verified with compiler explorer). Force inline */
-    __attribute__((always_inline)) InputIt get() const { return workIt; }
+    [[gnu::always_inline]] InputIt get() const { return workIt; }
 
-    __attribute__((always_inline)) CycleIt operator++(int) {
+    [[gnu::always_inline]] CycleIt operator++(int) {
         CycleIt temp = *this;
         ++*this;
         return temp;
     }
-    __attribute__((always_inline)) CycleIt& operator++() {
+    [[gnu::always_inline]] CycleIt& operator++() {
         if (++workIt == utl::end(buffer))
             workIt = utl::begin(buffer);
         return *this;
     }
-    __attribute__((always_inline)) size_t operator-(const CycleIt& rhs) {
+    [[gnu::always_inline]] size_t operator-(const CycleIt& rhs) {
         if (workIt >= rhs.workIt)
             return (workIt - rhs.workIt);
         else
             return (utl::size(buffer) - (rhs.workIt - workIt));
     }
-    __attribute__((always_inline)) auto& operator*() const { return *workIt; }
+    [[gnu::always_inline]] auto& operator*() const { return *workIt; }
 
 protected:
     Container& buffer;

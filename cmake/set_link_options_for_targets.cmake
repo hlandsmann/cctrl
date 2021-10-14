0f9e637117e892d1f8d9148671b84b787c9767d1
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2021-10-14 10:02:18 +0200

    update link options

--- cmake/set_link_options_for_targets.cmake
+++ cmake/set_link_options_for_targets.cmake
@@ -4,8 +4,7 @@ get_property(link_level GLOBAL PROPERTY GLOBAL_LINK_OPTIONS)
 foreach( target ${all_targets} )
     get_target_property(target_type ${target} TYPE)
     if ( target_type STREQUAL "EXECUTABLE" OR
-         target_type STREQUAL "STATIC_LIBRARY" OR
-         target_type STREQUAL "SHARED_LIBRARY")
+         target_type STREQUAL "STATIC_LIBRARY")
         message(STATUS "Applying link options for ${target_type} ${target} as per toolchain")
         target_link_options(${target} PRIVATE ${link_level})
     endif ()

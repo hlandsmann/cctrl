5da708675c3a819ab6f743fa1ecfa5e645f674b0
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2021-10-15 14:40:17 +0200

    stolen bitpattern from jason turners cppweekly

---
+++ libraries/utl/include/utl/bitpattern.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+namespace utl {
+
+class BitPattern {
+private:
+    // expected is the expected result after masking
+    std::uint16_t expected{};
+
+    // mask is set of bits I care about
+    std::uint16_t mask{0xFFFF};
+
+    // input & mask == expected
+
+public:
+    template <std::size_t Size> explicit consteval BitPattern(const char (&input)[Size]) {
+        std::uint16_t cur_bit = 1;
+        cur_bit <<= (Size - 2);
+
+        for (const char val : input) {
+            if (val == 0) {
+                return;
+            }
+
+            if (val == '1') {
+                expected |= cur_bit;
+            } else if (val == '0') {
+            } else if (val == '-') {
+                // 000010 // cur_bit
+                // 111101 // bitwise complement
+
+                // 110111 // hypothetical current mask
+                //&111101 // bitwise and
+                //=110101 // new mask, with cur_bit cleared from it
+                mask &= ~cur_bit;
+            } else {
+                using error_message = const char [];
+                new error_message("Unknown characters in BitPattern input");
+            }
+
+            cur_bit >>= 1;
+        }
+    }
+
+    template <typename ValueType>
+    constexpr friend bool operator==(const ValueType value,
+                                     const BitPattern &pattern) requires(std::is_unsigned_v<ValueType>) {
+        return (value & pattern.mask) == pattern.expected;
+    }
+};
+}  // namespace utl
